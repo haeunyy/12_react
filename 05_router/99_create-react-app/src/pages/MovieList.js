ea857dbc23449ad7f1b49139c2be03eb6865f86a
haeunyy <ibbihe@gmail.com>
2023-04-25 20:59:39 +0900

    commit

--- 05_router/99_create-react-app/src/pages/MovieList.js
+++ 05_router/99_create-react-app/src/pages/MovieList.js
@@ -5,6 +5,7 @@ import MovieItem from "../components/MovieItem";
 function MovieList() {
   const [movieList, setMovieList] = useState();
   const [value, setValue] = useState("");
+  const [btnValue, setBtnValue] = useState("");
 
   const onChange = (e) => {
     setValue(e.target.value);
@@ -13,17 +14,28 @@ function MovieList() {
   useEffect(() => {
     getMovieList(value).then((data) => setMovieList(data));
     console.log(getMovieList());
-  }, []);
+  }, [value]);
+
+  const onChangeHandler = (e) => {
+    setBtnValue(e.target.value);
+    console.log(e.target.value);
+  };
 
   return (
     <div>
       <h2>주간/주말 박스오피스 : </h2>
+      <input type="date" onChange={onChangeHandler} />
       <select value={value} onChange={onChange}>
         <option value="0">주간</option>
         <option value="1">주말</option>
         <option value="2">주중</option>
       </select>
-      <div className="content-row">{movieList}</div>
+      <div className="content-row">
+        {movieList &&
+          movieList.map((movie) => (
+            <MovieItem key={movie.movieCd} movie={movie} />
+          ))}
+      </div>
     </div>
   );
 }
